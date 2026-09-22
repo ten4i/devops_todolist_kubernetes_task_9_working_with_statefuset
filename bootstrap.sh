cf5eaf60c60ca9f11a462736021b9086564b44b1
ten4i <oleksijchunihin@gmail.com>
2026-09-23 00:18:39 +0300

    solution by ten4i

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+files=(
+    "namespace.yml"
+    "secret.yml"
+    "configMap.yml"
+    "configMapMysql.yml"
+    "pv.yml"
+    "pvc.yml"
+    "deployment.yml"
+    "mysqlHeadless.yml"
+    "statefulSet.yml"
+    "nodeport.yml"
+    "clusterIp.yml"
+    "hpa.yml"
+) 
+
+for file in "${files[@]}"; do
+    kubectl apply -f .infrastructure/$file
+done
+
